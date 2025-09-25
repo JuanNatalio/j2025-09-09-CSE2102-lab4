d376f45fc1b76e041165d1fcd53a83fd7e54a522
JuanNatalio <jonathanjimenez446138@gmail.com>
2025-09-25 19:23:17 +0000

    added API tests

--- test_factors.py
+++ test_factors.py
@@ -1,5 +1,5 @@
 import unittest
-from my_server import trial_division
+from my_server import trial_division, app
 
 class TestTrialDivision(unittest.TestCase):
     def test_factors_of_12(self):
@@ -13,6 +13,31 @@ class TestTrialDivision(unittest.TestCase):
 
     def test_factors_of_1(self):
         self.assertEqual(trial_division(1), [1])
+
+
+class TestFlaskEndpoints(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_post_factors_12(self):
+        response = self.client.post('/factors', data={'inINT': '12'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json(), {"factors": [2, 2, 3]})
+
+    def test_post_factors_prime(self):
+        response = self.client.post('/factors', data={'inINT': '13'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json(), {"factors": [13]})
+
+    def test_post_factors_1(self):
+        response = self.client.post('/factors', data={'inINT': '1'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json(), {"factors": [1]})
+
+    def test_post_factors_missing_param(self):
+        response = self.client.post('/factors', data={})
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("error", response.get_json())
 
 if __name__ == "__main__":
     unittest.main()
